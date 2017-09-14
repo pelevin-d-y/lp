1bb0cdaefb30b1da233fa742b40103f702d9b4a4
natpuot92 <natpuot1992@gmail.com>
2017-09-14 16:42:40 +0300

    clients-slider

--- app/initialize.js
+++ app/initialize.js
@@ -1,36 +1,9 @@
 import $ from "jquery"
 import slick from 'slick-carousel'
 
-function initSlick () {
-  $('.features-list').slick({
-    infinite: true,
-    arrows: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-    ]
+$(document).ready(function(){
+  $('.clients__slider').slick({
   })
-}
-
-$(document).ready(() => {
-  initSlick()
 })
 
 
